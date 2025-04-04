acb8acdd01c5e16a46a18e3930fdf32aa7ba78f7
FarahTrigui <farahtrigui5@gmail.com>
2025-04-04 02:21:52 +0100

    setting-up-the-iac

--- main.tf
+++ main.tf
@@ -1,21 +1,106 @@
 # Configure the Azure provider
 provider "azurerm" {
+  resource_provider_registrations = "none"
   features {}
 }
 
 # Create a Resource Group
 resource "azurerm_resource_group" "rg" {
-  name     = "rg-terraform-vm"
+  name     = "rg-terraform"
   location = "East US"
 }
 
 # Create a Virtual Network
 resource "azurerm_virtual_network" "vnet" {
-  name                = "vnet-terraform-vm"
+  name                = "vnet-terraform"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
 
 # Create a Subnet
-resource "azurerm
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet-terraform"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# Create a Public IP
+resource "azurerm_public_ip" "public_ip" {
+  name                = "pip-terraform"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Dynamic"
+  sku                 = "Basic"
+}
+
+
+# Create a Network Interface
+resource "azurerm_network_interface" "nic" {
+  name                = "nic-terraform-vm"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.public_ip.id
+  }
+}
+
+# Generate an SSH Key Pair (optional if you haven't generated one)
+# Alternatively, use your pre-generated key.
+resource "tls_private_key" "ssh_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+# Save the public key to a file (you can also output it as a Terraform output)
+resource "local_file" "ssh_public_key" {
+  content  = tls_private_key.ssh_key.public_key_openssh
+  filename = "${path.module}/id_rsa.pub"
+}
+
+# Create an Ubuntu Linux Virtual Machine
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "vm-terraform"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B1s"
+  admin_username      = "azureuser"
+  
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
+
+  # Use the public key generated above or provide your own
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = tls_private_key.ssh_key.public_key_openssh
+  }
+
+  # Use the latest Ubuntu LTS image
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+}
+
+# Outputs
+output "public_ip_address" {
+  value = azurerm_public_ip.public_ip.ip_address
+}
+
+output "ssh_private_key_path" {
+  value = "${path.module}/id_rsa"
+}
+
